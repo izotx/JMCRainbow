103135366aa6df11c48e2bc3a2880b5bad410a64
Janusz Chudzynski <appzzman>
2015-09-30 17:28:40 -0500

    Created UIImage extension

--- RainbowPlayground.playground/Contents.swift
+++ RainbowPlayground.playground/Contents.swift
@@ -13,44 +13,45 @@
 
 import UIKit
 
-/**
+extension UIImage{
+    
+    /**
     size: target size of the image
     segments: number of segments in the hue
-*/
-func generateRainbow(size:CGSize, segments:Int)->UIImage{
-
-    let segmentWidth = size.width / CGFloat(segments)
-
-    UIGraphicsBeginImageContext(size)
-    let context = UIGraphicsGetCurrentContext()
+    */
     
-    for var i:Int = 0; i < segments; i = i+1{
-        //generate color
-        let currentHue = CGFloat(i)/CGFloat(segments)
-        let color =  UIColor(hue: currentHue, saturation: 1, brightness: 1, alpha: 1.0)
-        //current x offset
-        let currentx:CGFloat = size.width * CGFloat(i) / CGFloat(segments)
+    static func generateRainbow(size:CGSize, segments:Int)->UIImage{
+        let segmentWidth = size.width / CGFloat(segments)
         
-        CGContextSetFillColorWithColor(context, color.CGColor)
-        CGContextFillRect(context, CGRectMake(currentx, 0, segmentWidth, size.height))
+        UIGraphicsBeginImageContext(size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        for var i:Int = 0; i < segments; i = i+1{
+            //generate color
+            let currentHue = CGFloat(i)/CGFloat(segments)
+            let color =  UIColor(hue: currentHue, saturation: 1, brightness: 1, alpha: 1.0)
+            //current x offset
+            let currentx:CGFloat = size.width * CGFloat(i) / CGFloat(segments)
+            
+            CGContextSetFillColorWithColor(context, color.CGColor)
+            CGContextFillRect(context, CGRectMake(currentx, 0, segmentWidth, size.height))
+        }
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        
+        UIGraphicsEndImageContext()
+        return image
+        
     }
-    
-let image = UIGraphicsGetImageFromCurrentImageContext()
-   
-    
-UIGraphicsEndImageContext()
-    return image
 }
 
-
-generateRainbow(CGSizeMake(600, 100),1000)
 
 
 extension UIView{
     func rainbowBackground(){
-       self.backgroundColor = UIColor(patternImage: generateRainbow(self.frame.size, 1000))
+        self.backgroundColor = UIColor(patternImage: UIImage.generateRainbow(self.frame.size, segments: 1000))
     }
-
 }
 
 
@@ -58,6 +59,12 @@ extension UIView{
 let v = UIView(frame: CGRectMake(0, 0, 1000, 1000))
 v.rainbowBackground()
 
+let slider = UISlider(frame: CGRectMake(0, 0, 100, 10))
+//slider.rainbowBackground()
+let rainbow = UIImage.generateRainbow(CGSizeMake(slider.frame.width, 2), segments: 1000).stretchableImageWithLeftCapWidth(7, topCapHeight: 0)
 
+
+slider.setMinimumTrackImage(rainbow, forState: UIControlState.Normal)
+slider.setMaximumTrackImage(rainbow, forState: UIControlState.Normal)
 
 
